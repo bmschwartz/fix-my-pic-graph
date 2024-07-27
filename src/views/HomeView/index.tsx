266c868fff7598ab7140e82dbaecee098db704f8
Ben Schwartz <schwartz.ben0@gmail.com>
2024-07-27 11:03:29 -0400

    Adding graphql stuff

--- src/views/HomeView/index.tsx
+++ src/views/HomeView/index.tsx
@@ -1,7 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+
+import {
+  execute,
+  GetPictureRequestsDocument,
+  GetPictureRequestsQuery,
+  PictureRequest as GqlPictureRequest,
+} from '@/graphql/client';
 
 const HomeView = () => {
-  return <div>Hi!</div>;
+  const [pictureRequestData, setPictureRequestData] = React.useState<GetPictureRequestsQuery>();
+  const [pictureRequests, setPictureRequests] = React.useState<GqlPictureRequest[]>([]);
+
+  useEffect(() => {
+    execute(GetPictureRequestsDocument, {}).then((result) => {
+      setPictureRequests(result?.data?.pictureRequests || []);
+    });
+  }, [setPictureRequestData]);
+
+  return (
+    <div>
+      {pictureRequests && (
+        <ul>
+          {pictureRequests.map((pictureRequest) => (
+            <li key={pictureRequest.id}>{pictureRequest.title}</li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
 };
 
 export default HomeView;
