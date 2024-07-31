ea689c2efe111eb4ed29ed6082100ea5212b2d66
Ben Schwartz <schwartz.ben0@gmail.com>
2024-07-31 17:04:24 -0400

    Lots of changes

--- contracts/RequestSubmission/BaseRequestSubmission.sol
+++ contracts/RequestSubmission/BaseRequestSubmission.sol
@@ -3,13 +3,11 @@ pragma solidity ^0.8.0;
 
 import '@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol';
 import '@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol';
+import '@chainlink/contracts/src/v0.8/interfaces/FeedRegistryInterface.sol';
+import {Denominations} from '@chainlink/contracts/src/v0.8/Denominations.sol';
 
 contract BaseRequestSubmission is Initializable, ReentrancyGuardUpgradeable {
-  event SubmissionPurchased(
-    address indexed submission,
-    address indexed purchaser,
-    uint256 purchaseDate
-  );
+  event SubmissionPurchased(address indexed submission, address indexed purchaser, uint256 purchaseDate);
 
   address public request;
   address public submitter;
@@ -20,6 +18,9 @@ contract BaseRequestSubmission is Initializable, ReentrancyGuardUpgradeable {
   string public encryptedImageId;
   string public watermarkedImageId;
   mapping(address => bool) public submissionPurchasers;
+
+  FeedRegistryInterface internal priceFeed;
+  address internal ETH_PRICE_FEED_ADDRESS = 0x47Fb2585D2C56Fe188D0E6ec628a38b74fCeeeDf;
 
   function initialize(
     address _request,
@@ -40,13 +41,22 @@ contract BaseRequestSubmission is Initializable, ReentrancyGuardUpgradeable {
     encryptedImageId = _encryptedImageId;
     submitter = _submitter;
     createdAt = block.timestamp;
+    priceFeed = FeedRegistryInterface(ETH_PRICE_FEED_ADDRESS);
+  }
+
+  function getLatestETHPrice() public view returns (uint256) {
+    (, int ethPrice, , , ) = priceFeed.latestRoundData(Denominations.ETH, Denominations.USD);
+    return uint256(ethPrice) * 1e10; // Adjust for decimals
   }
 
   function purchaseSubmission() external payable nonReentrant {
-    require(msg.value >= price, 'Insufficient payment');
+    uint256 ethPriceInUsd = getLatestETHPrice();
+    uint256 priceInWei = (price * 1e18) / ethPriceInUsd;
+
+    require(msg.value >= priceInWei, 'Insufficient payment');
     require(!submissionPurchasers[msg.sender], 'Already purchased');
 
-    (bool success, ) = submitter.call{ value: msg.value }('');
+    (bool success, ) = submitter.call{value: msg.value}('');
     require(success, 'Payment failed');
 
     submissionPurchasers[msg.sender] = true;
