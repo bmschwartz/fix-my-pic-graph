754e3a73ad218c9906b010a94f131f9d5d07340c
Ben Schwartz <schwartz.ben0@gmail.com>
2024-07-31 06:14:18 -0400

    Lots of changes

--- src/pages/request/[id].tsx
+++ src/pages/request/[id].tsx
@@ -1,21 +1,59 @@
 import React from 'react';
 
-import { execute } from '@/graphql/client';
+import { execute, GetPictureRequestDocument } from '@/graphql/client';
 import { PictureRequest } from '@/types/pictureRequest';
+import { getLogger } from '@/utils/logging';
 import RequestDetailView from '@/views/request/RequestDetailView';
 
 interface RequestDetailsPageProps {
-  request: PictureRequest;
+  request: PictureRequest | null;
+  error?: string;
 }
 
-const RequestDetailsPage: React.FC<RequestDetailsPageProps> = ({ request }) => {
+const logger = getLogger('RequestDetailsPage');
+
+const RequestDetailsPage: React.FC<RequestDetailsPageProps> = ({ request, error }) => {
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
+  if (!request) {
+    return <div>Loading...</div>;
+  }
+
   return <RequestDetailView request={request} />;
 };
 
 export async function getServerSideProps(context: any) {
-  const { id } = context.params;
+  try {
+    const { id } = context.params;
 
-  const request = await execute(GetPictureRequestDocument, { id });
+    const result = await execute(GetPictureRequestDocument, { id });
+
+    if (!result?.data?.request) {
+      logger.error('Request not found', Object.keys(result?.data));
+      return {
+        props: {
+          request: null,
+          error: 'Request not found',
+        },
+      };
+    }
+
+    return {
+      props: {
+        request: result.data.request,
+      },
+    };
+  } catch (error) {
+    logger.error('Failed to fetch picture request:', error);
+    return {
+      props: {
+        request: null,
+        error: 'Failed to fetch picture request',
+      },
+    };
+  }
 }
 
 export default RequestDetailsPage;
